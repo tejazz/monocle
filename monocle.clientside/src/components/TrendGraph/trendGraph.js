f525f984cf598e399f209b200d0f18e329730e00
tejaz <Tarique.Ejaz@agoda.com>
2021-06-14 17:50:50 +0700

    feat: add temperory chart structure

--- monocle.clientside/src/components/TrendGraph/trendGraph.js
+++ monocle.clientside/src/components/TrendGraph/trendGraph.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import * as Recharts from 'recharts';
+import {AreaChart, Area, XAxis, YAxis, Tooltip} from 'recharts';
 import './trendGraph.scss';
 
-function TrendGraph() {
-    return (<div>Trend Graph</div>);
+function TrendGraph(props) {
+    return (
+        <div className='trend'>
+            <p className='trend__title'>Trend Graph</p>
+
+            <div>
+                <AreaChart
+                    width={730}
+                    height={250}
+                    data={props.trends}
+                    margin={{
+                        top: 20, right: 20, bottom: 20, left: 20,
+                    }}
+                >
+                    <XAxis dataKey="timestamp" />
+                    <YAxis />
+                    <Area dataKey="positive" stroke="#8884d8" fill="#8884d8" />
+                    <Area dataKey="negative" stroke="#8884d8" fill="#8884d8" />
+                    <Area dataKey="neutral" stroke="#8884d8" fill="#8884d8" />
+                    <Tooltip />
+                </AreaChart>
+            </div>
+        </div>
+    );
 }
 
 export default React.memo(TrendGraph);
