8300d5f8d05388a3f7ae2a21f4b5842a744becb0
tejaz <Tarique.Ejaz@agoda.com>
2021-06-25 17:55:16 +0700

    feat: rerender graph overall statistics for trend

--- monocle.clientside/src/components/TrendGraph/trendGraph.js
+++ monocle.clientside/src/components/TrendGraph/trendGraph.js
@@ -3,24 +3,25 @@ import { AreaChart, Area, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'rec
 import './trendGraph.scss';
 
 const TrendGraph = ({data}) => {
-    console.log(data);
+    let dataSegment = data.slice(data.length - 1000);
+
     return (
         <div className='trend'>
             <p className='trend__title'>Trend Graph</p>
 
             <div>
-                <ResponsiveContainer width="100%" height={500}>
+                <ResponsiveContainer width="100%" height={500} key={Math.random()}>
                     <AreaChart
-                        data={data}
+                        data={dataSegment}
                         margin={{
                             top: 20, right: 20, bottom: 20, left: 20,
                         }}
                     >
                         <XAxis dataKey="timestamp" />
                         <YAxis />
-                        <Area dataKey="positive" stroke="green" fill="green" />
-                        <Area dataKey="negative" stroke="red" fill="red" />
-                        <Area dataKey="neutral" stroke="yellow" fill="yellow" />
+                        <Area isAnimationActive={false} dataKey="positive" stroke="#6cad46" fill="#8bde5a" strokeWidth={3} fillOpacity={0.3}/>
+                        <Area isAnimationActive={false} dataKey="negative" stroke="#d44842" fill="#fe6f5e" strokeWidth={3} fillOpacity={0.3}/>
+                        <Area isAnimationActive={false} dataKey="neutral" stroke="#eeb700" fill="#fff23e" strokeWidth={3} fillOpacity={0.3}/>
                         <Tooltip />
                     </AreaChart>
                 </ResponsiveContainer>
